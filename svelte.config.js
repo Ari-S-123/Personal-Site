8d054c6336b6125b786e1e2b9cccbb5b6ad57d1d
Aritra (Ari) Saharay <80832201+Ari-S-123@users.noreply.github.com>
2025-05-25 11:32:39 -0700

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
   kit: {
     adapter: adapter({
       runtime: "nodejs22.x",
-      regions: ["sfo1", "pdx1", "iad1", "cle1", "dub1", "lhr1", "bom1", "sin1", "hkg1", "syd1", "cpt1"],
+      regions: ["sfo1"],
       images: {
         sizes: [640, 828, 1200, 1920, 3840],
         formats: ["image/avif", "image/webp"],
